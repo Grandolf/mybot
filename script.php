8aad79774d4059b6706f8a365cdefec110616e04
Grandolf <Grandolf2000@gmail.com>
2017-06-30 13:59:33 -0400

    Update script.php

--- script.php
+++ script.php
@@ -1,33 +1,30 @@
-
 <?php
-
 # This is adapted from:
 # http://www.wikihow.com/Develop-an-IRC-Bot
-
 # To run me,  fill in the parameters.  Then do  php filename.php5 under
 # Linux.
-$server   = "188.165.239.81";
+$server   = "irc.oldcoder.org";
 $port     = 6667;
-$nickname = 'Grandbot2';
-$ident    = 'alwaystalking';
+$nickname = 'GrandbotOld';
+$ident    = 'YouShallNotPass';
 $gecos    = 'diebot to murder me';
-$channel  = "#XD";
-
+$channel  = "#minetest-general";
+function simpletalk ($text)
+	{
+	global $d, $socket;
+	socket_write ($socket, 'PRIVMSG ' . $d [2] . " :$text\r\n");
+	}
 # Set up a network socket connection to the IRC server
-
 $socket = socket_create  (AF_INET, SOCK_STREAM, SOL_TCP);
 $error  = socket_connect ($socket, $server, $port);
-
 if ($socket === false) {
 	$errorCode   = socket_last_error();
 	$errorString = socket_strerror ($errorCode);
 	die ("Error $errorCoder: $errorString\n");
 }
-
 # Do a basic login (not authenticated)
 socket_write ($socket, "NICK $nickname\r\n");
 socket_write ($socket, "USER $ident * 8 :$gecos\r\n");
-
 # Now lurkey like a turkey
 while (is_resource ($socket))
 {
@@ -35,21 +32,20 @@ while (is_resource ($socket))
 	echo $data . "\n";
 	$d    = explode (' ', $data);
 	$d    = array_pad ($d, 10, "");
-
+	echo "[3] ".$d[3]."\n";
+	echo "[4] ".$d[4]."\n";
+	echo "[5] ".$d[5]."\n";
     if ($d [0] === 'PING')
     { # Need to respond to pings
     	socket_write ($socket, 'PONG' . $d [1] . "\r\n");
     }
-
     if (($d [1] === '376') || ($d [1] === '422'))
     { # Join channel when things are ready
     	socket_write ($socket, 'JOIN ' . $channel . "\r\n");
     }
-
     if ($d [3] == ":diebot")
-    {	$text = 'noooooo'; socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n");
+    {simpletalk ('We are but bubbles in the foam');
 	}
-
     if ($d [3] == ':@moo')
     {
         $moo = "M" . str_repeat ("o", mt_rand (2, 15));
@@ -60,9 +56,8 @@ while (is_resource ($socket))
         $moo = "h" . str_repeat ("i", mt_rand (1, 3));
     	socket_write ($socket, 'PRIVMSG ' . $d [2] . " :$moo\r\n");
 	}
-    if ($d [3] == ':bye')
 	if ($d [3] == ':bye') {
-	$text = 'buh bye'; socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
+	simpletalk ('buh buy'); 
 	}
 	if ($d [3] == ':bacon') {
 	$text = 'Fry it'; socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
@@ -87,7 +82,7 @@ while (is_resource ($socket))
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
 	if (($d [3] == ':***') and ($d [5] == 'joined') and ($d [6] == 'the') and ($d [7] == 'game')) {
-	$text = "sorry, servers are currently expiriencing tecnical issues, they will be resolved ASAP, please come back later" . str_repeat (":)", mt_rand (1, 3));;
+	$text = "hi" . str_repeat (":)", mt_rand (1, 3));;
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
 	if (($d [3] == ':when') and ($d [4] == 'is') and ($d [5] == 'your') and ($d [6] == 'birthday')) {
@@ -97,6 +92,9 @@ while (is_resource ($socket))
 	if (($d [1] == 'ChanServ') and ($d [2] == 'gives') and ($d [3] == 'voice') and ($d [4] == 'to')) {
 	$text = 'Hi :D';
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n");
+	}
+	if ($d [1] == 'JOIN') {
+	$text = 'hi :)'; socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
     if ($d [3] == ":diebot")
     {	exit (1) and exit (2);
@@ -152,8 +150,6 @@ while (is_resource ($socket))
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
 	# {,,},{,,},{,,}
-
-
 # mods
 	# 3d_armor
 	if ($d [3] == ':?3d_armor') {
@@ -180,47 +176,37 @@ while (is_resource ($socket))
 	$text = 'adds armors, equipt armors by placing the pieces of armor in armor slots{x,x,x},{x,x,x},{"",x,""}';
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	# Orienteering v1.2
-
 	if ($d [3] == ':?orienteering') {
 	$text = 'A collection of tools to help you orient yourselves in the world, such as compass, altimeter, etc. commands are: ?orienteering:altimeter ?orienteering:triangulator ?orienteering:compass ?orienteering:sextant ?orienteering:quadcorder ?orienteering:watch ?orienteering:speedometer ?orienteering:automapper ?orienteering:gps';
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:altimeter') {
 	$text = 'It shows you your current elevation (Y);recipe: {"default:glass"},{"default:steel_ingot"},{"default:steel_ingot"},'; 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:triangulator') {
 	$text = 'shows you the coordinates of your current position in the horizontal plane (X and Z);recipe = {"", "default:bronze_ingot", ""}, {"default:bronze_ingot", "", "default:bronze_ingot"}, '; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:compass') {
 	$text = 'It shows you your yaw (horizontal viewing angle) in degrees; recipe = {"", "default:copper_ingot", ""}, {"default:copper_ingot", "group:stick", "default:copper_ingot"}, {"", "default:copper_ingot", ""}, '; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:sextant') {
 	$text = 'It shows you your pitch (vertical viewing angle) in degrees; recipe = {"", "default:gold_ingot", ""},{"default:gold_ingot", "default:gold_ingot", "default:gold_ingot"}, '; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:quadcorder') {
 	$text = 'This is the ultimate orientieering tool. It shows you your coordinates (X, Y and Z), shows your yaw and pitch (horizontal and vertical viewing angles), the current time, your current speed and it enables you to access the minimap; recipe = { "default:gold_ingot", "default:gold_ingot", "default:gold_ingot" }, { "orienteering:speedometer", "default:diamond", "orienteering:automapper", }, { "orienteering:sextant", "default:diamond", "orienteering:gps" }'; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:watch') {
 	$text = 'It shows you the current time; Put the watch in your hotbar to see the time. Leftclick to toggle between the 24-hour and 12-hour display; recipe = { "default:copper_ingot" }, { "default:glass" }, { "default:copper_ingot" }'; 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:speedometer') {
 	$text = 'It shows you your current horizontal (“hor.”) and vertical (“ver.”) speed in meters per second, where one meter is the side length of a single cube; recipe = {"", "default:gold_ingot", ""}, {"default:steel_ingot", "group:stick", "default:steel_ingot"}, {"", "default:steel_ingot", ""},'; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?orienteering:automapper') {
 	$text = 'The automapper automatically creates a map of the area around you and enables you to view a minimap of your surroundings. It also has a built-in radar; If you put an automapper in your hotbar, you will be able to access the minimap. By default the minimap can be opened with [F7];
 		recipe = {
@@ -245,42 +231,10 @@ while (is_resource ($socket))
 	$text = ''; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
 	if ($d [3] == ':?') {
 	$text = ''; 
 	socket_write ($socket, 'PRIVMSG ' . $d[2] . " :$text\r\n"); 
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 }
 ?>
